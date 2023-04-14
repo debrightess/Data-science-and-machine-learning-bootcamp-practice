2ac03c8308c283a220fa5c53886e723845a4d99e
Bright Tenalo <debrightess@yahoo.com>
2023-04-14 10:27:09 +0100

    module task completed

--- life.py
+++ life.py
@@ -1,1 +1,14 @@
+import math
+
 theAnswer = 42
+
+def quote_marvin():
+    print('I\'ve calculated your chance of survival, but i don\'t think you\'ll like it.')
+
+
+def square_root(number):
+    return math.sqrt(number)
+
+squared_num = square_root(63.14)
+
+print(squared_num)
